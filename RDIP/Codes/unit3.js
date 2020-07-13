27bef94a96c1a44b1ec8547a34c7c8750544b2da
shiva-099 <58987219+shiva-099@users.noreply.github.com>
2020-07-13 17:51:30 +0530

    goa1 8,9

--- RDIP/Codes/unit3.js
+++ RDIP/Codes/unit3.js
@@ -2,8 +2,18 @@
 var reforms;
 var length=0;
 var count=0;
+var x;
+var selectedSentence;
+var e1,e2,e3,e4,e5,e6,e7,e8,e9,e10;
+var h1,h2,h3,h4,h5,h6,h7;
+var all_hin=[];
+var all_eng=[];
+var arr;
+var arra;
+var arrs;
+var array;
 function select() {
-  var x=document.getElementById('select').value;
+   x=document.getElementById('select').value;
   if(x=="English"||x=="Hindi")
   {
     document.getElementById("Long").innerHTML="Form a sentence (Declarative or Interrogative or any other type) from the given words";
@@ -26,29 +36,29 @@ function ran()
   {
 
 
-            var e1 = ['John ate an apple before afternoon',
+              e1 = ['John ate an apple before afternoon',
                             'before afternoon John ate an apple',
                             'John before afternoon ate an apple'
                             ];
-            var e2 = ['some students like to study in the night',
+              e2 = ['some students like to study in the night',
                             'at night some students like to study'
                            ];
-            var e3 = ['John and Mary went to church',
+              e3 = ['John and Mary went to church',
                             'Mary and John went to church'];
-            var e4 = ['John went to church after eating',
+              e4 = ['John went to church after eating',
                             'after eating John went to church',
                             'John after eating went to church'];
-            var e5 = ['did he go to market',
+              e5 = ['did he go to market',
                             'he did go to market'];
-            var e6 = ['the womean who called my sister sells cosmetics',
+              e6 = ['the womean who called my sister sells cosmetics',
                             'the woman who sells cosmetics called my sister',
                             'my sister who sells cosmetics called the woman',
                             'my sister who called the woman sells cosmetics'];
-            var e7 = ['John goes to the library and studies',
+              e7 = ['John goes to the library and studies',
                             'John studies and goes to the library'];
-            var e8 = ['John ate an apple so did she',
+              e8 = ['John ate an apple so did she',
                             'she ate an apple so did John'];
-            var e9 = ['the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌ ‌after‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌',
+             e9 = ['the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌ ‌after‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌',
                             'the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌ ‌after‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌',
                             'after‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌',
                             'after‌ ‌the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌',
@@ -56,7 +66,7 @@ function ran()
                             'she‌ ‌noticed‌ ‌the‌ ‌error‌ ‌after‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌',
                             'after‌ ‌she‌ ‌returned‌ ‌the‌ ‌book‌ ‌the‌ ‌teacher‌ ‌noticed‌ ‌the‌ ‌error‌',
                             'after‌ ‌she‌ ‌noticed‌ ‌the‌ ‌error‌ ‌the‌ ‌teacher‌ ‌returned‌ ‌the‌ ‌book‌'];
-            var e10 = ['I‌ ‌told‌ ‌her‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌yesterday‌',
+             e10 = ['I‌ ‌told‌ ‌her‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌yesterday‌',
                              'I‌ ‌told‌ ‌her‌ ‌yesterday‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌',
                              'yesterday‌ ‌I‌ ‌told‌ ‌her‌ ‌that‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌',
                              'I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌ ‌yesterday‌',
@@ -64,8 +74,8 @@ function ran()
                              'yesterday‌ ‌I‌ ‌bought‌ ‌a‌ ‌book‌ ‌that‌ ‌I‌ ‌told‌ ‌her‌'];
            var all_eng=[e1,e2,e3,e4,e5,e6,e7,e8,e9,e10];
 
-      var arr  =all_eng[Math.floor(Math.random() * all_eng.length)];
-      var arra =arr[Math.floor(Math.random() * arr.length)];
+        arr  =all_eng[Math.floor(Math.random() * all_eng.length)];
+        arra =arr[Math.floor(Math.random() * arr.length)];
       var bb=arra.split(" ");
       for(i=0;i<bb.length;i++)
         {
@@ -84,20 +94,23 @@ function ran()
           document.getElementById("selected").innerHTML ="";
           document.getElementById("reform").innerHTML = "";
            document.getElementById("checkcorrectness").innerHTML="";
+           document.getElementById("correct").innerHTML="";
+           document.getElementById("wrong").innerHTML="";
+           document.getElementById("showAnswer").innerHTML=""
     }
     reforms=document.getElementById("sen").innerHTML ;
 }
          if(x=="Hindi")
         {
-            var h1 = ['राम‌ ‌और‌ ‌श्याम‌ ‌बाजार‌ ‌गयें‌',
+              h1 = ['राम‌ ‌और‌ ‌श्याम‌ ‌बाजार‌ ‌गयें‌',
                         'राम‌ ‌और‌ ‌श्याम‌ ‌गयें‌ ‌बाजार‌',
                         'बाजार‌ ‌गयें‌ ‌राम‌ ‌और‌ ‌श्याम‌',
                         'गयें‌ ‌बाजार‌ ‌राम‌ ‌और‌ ‌श्याम‌'];
-            var h2 = ['राम‌ ‌सोया‌ ‌और‌ ‌श्याम‌ ‌भी‌',
+              h2 = ['राम‌ ‌सोया‌ ‌और‌ ‌श्याम‌ ‌भी‌',
                         'श्याम‌ ‌सोया‌ ‌और‌ ‌राम‌ ‌भी‌',
                         'सोया‌ ‌श्याम‌ ‌और‌ ‌राम‌ ‌भी‌',
                         'सोया‌ ‌राम‌ ‌और‌ ‌श्याम‌ ‌भी‌'];
-            var h3 = ['मैंने‌ ‌उसे‌ ‌बताया‌ ‌कि‌ ‌राम‌ ‌सो‌ ‌रहा‌ ‌है‌',
+              h3 = ['मैंने‌ ‌उसे‌ ‌बताया‌ ‌कि‌ ‌राम‌ ‌सो‌ ‌रहा‌ ‌है‌',
                         'मैंने‌ ‌उसे‌ ‌बताया‌ ‌कि‌ ‌सो‌ ‌रहा‌ ‌है‌ ‌राम‌',
                         'उसे‌ ‌मैंने‌ ‌बताया‌ ‌कि‌ ‌राम‌ ‌सो‌ ‌रहा‌ ‌है‌',
                         'उसे‌ ‌मैंने‌ ‌बताया‌ ‌कि‌ ‌सो‌ ‌रहा‌ ‌है‌ ‌राम‌',
@@ -109,13 +122,13 @@ function ran()
                         'बताया‌ ‌मैंने‌ ‌उसे‌ ‌कि‌ ‌सो‌ ‌रहा‌ ‌है‌ ‌राम‌',
                         'बताया‌ ‌उसे‌ ‌मैंने‌ ‌कि‌ ‌राम‌ ‌सो‌ ‌रहा‌ ‌है‌',
                         'बताया‌ ‌उसे‌ ‌मैंने‌ ‌कि‌ ‌सो‌ ‌रहा‌ ‌है‌ ‌राम‌'];
-            var h4 = ['राम‌ ‌खाकर‌ ‌सोया‌',
+              h4 = ['राम‌ ‌खाकर‌ ‌सोया‌',
                         'खाकर‌ ‌राम‌ ‌सोया‌',
                         'राम‌ ‌सोया‌ ‌खाकर‌',
                         'खाकर‌ ‌सोया‌ ‌राम‌',
                         'सोया‌ ‌राम‌ ‌खाकर‌',
                         'सोया‌ ‌खाकर‌ ‌राम‌'];
-            var h5 = ['बिल्लियों‌ ‌को‌ ‌मारकर‌ ‌कुत्ता‌ ‌सो‌ ‌गया‌',
+              h5 = ['बिल्लियों‌ ‌को‌ ‌मारकर‌ ‌कुत्ता‌ ‌सो‌ ‌गया‌',
                         'मारकर‌ ‌बिल्लियों‌ ‌को‌ ‌कुत्ता‌ ‌सो‌ ‌गया',
                         'बिल्लियों‌ ‌को‌ ‌मारकर‌ ‌सो‌ ‌गया‌ ‌कुत्ता‌',
                         'मारकर‌ ‌बिल्लियों‌ ‌को‌ ‌सो‌ ‌गया‌ ‌कुत्ता‌',
@@ -123,11 +136,11 @@ function ran()
                         'कुत्ता‌ ‌सो‌ ‌गया‌ ‌मारकर‌ ‌बिल्लियों‌ ‌को‌',
                         'सो‌ ‌गया‌ ‌कुत्ता‌ ‌बिल्लियों‌ ‌को‌ ‌मारकर',
                         'सो‌ ‌गया‌ ‌कुत्ता‌ ‌मारकर‌ ‌बिल्लियों‌ ‌को‌'];
-            var h6 = ['एक‌ ‌लाल‌ ‌किताब‌ ‌वहाँ‌ ‌है‌',
+               h6 = ['एक‌ ‌लाल‌ ‌किताब‌ ‌वहाँ‌ ‌है‌',
                         'एक‌ ‌लाल‌ ‌किताब‌ ‌है‌ ‌वहाँ‌',
                         'वहाँ‌ ‌है‌ ‌एक‌ ‌लाल‌ ‌किताब',
                         'है‌ ‌वहाँ‌ ‌एक‌ ‌लाल‌ ‌किताब'];
-            var h7 = ['एक‌ ‌बड़ी‌ ‌सी‌ ‌किताब‌ ‌वहाँ‌ ‌है‌',
+              h7 = ['एक‌ ‌बड़ी‌ ‌सी‌ ‌किताब‌ ‌वहाँ‌ ‌है‌',
                         'एक‌ ‌बड़ी‌ ‌सी‌ ‌किताब‌ ‌है‌ ‌वहाँ‌',
                         'बड़ी‌ ‌सी‌ ‌एक‌ ‌किताब‌ ‌वहाँ‌ ‌है‌',
                         '‌बड़ी‌ ‌सी‌ ‌एक‌ ‌किताब‌ ‌है‌ ‌वहाँ',
@@ -135,9 +148,9 @@ function ran()
                         'वहाँ‌ ‌है‌ ‌बड़ी‌ ‌सी‌ ‌एक‌ ‌किताब‌',
                         '‌है‌ ‌वहाँ‌ ‌एक‌ ‌बड़ी‌ ‌सी‌ ‌किताब‌',
                         'है‌ ‌वहाँ‌ ‌बड़ी‌ ‌सी‌ ‌एक‌ ‌किताब‌'];
-          var all_hin=[h1,h2,h3,h4,h5,h6,h7];
-          var arr  =all_hin[Math.floor(Math.random() * all_hin.length)];
-          var array =arr[Math.floor(Math.random() * arr.length)];
+            all_hin=[h1,h2,h3,h4,h5,h6,h7];
+            arrs  =all_hin[Math.floor(Math.random() * all_hin.length)];
+            array =arrs[Math.floor(Math.random() * arrs.length)];
           var b2=array.split(" ");
           for(i=0;i<b2.length;i++)
             {
@@ -157,6 +170,9 @@ function ran()
              document.getElementById("selected").innerHTML ="";
              document.getElementById("reform").innerHTML = "";
              document.getElementById("checkcorrectness").innerHTML="";
+             document.getElementById("correct").innerHTML="";
+             document.getElementById("wrong").innerHTML="";
+             document.getElementById("showAnswer").innerHTML=""
         }
         reforms=document.getElementById("sen").innerHTML ;
       }
@@ -171,7 +187,7 @@ function ran()
           count++;
             if(length==count && length>0)
             {
-                document.getElementById("checkcorrectness").innerHTML="<button id='check'>Check Correctness of the Sentence</button>";
+                document.getElementById("checkcorrectness").innerHTML="<button id='check' onclick='check()'>Check Correctness of the Sentence</button>";
                 selectedSentence = document.getElementById("selected").innerHTML;
 
             }
@@ -188,6 +204,44 @@ function ran()
             document.getElementById("selected").innerHTML ="";
             document.getElementById("reform").innerHTML = "";
             document.getElementById("checkcorrectness").innerHTML="";
+            document.getElementById("correct").innerHTML="";
+            document.getElementById("wrong").innerHTML="";
+            document.getElementById("showAnswer").innerHTML=""
 
 
         }
+        function check()
+        {
+            var ans= selectedSentence.trim();
+            if(x=='English')
+            {
+                if (arr.includes(ans))
+                {
+                document.getElementById("correct").innerHTML="Correct Answer!!!";
+                document.getElementById("wrong").innerHTML="";
+                document.getElementById("showAnswer").innerHTML=""
+
+                }
+                else{
+                    document.getElementById("wrong").innerHTML="Wrong Answer !!!";
+                    document.getElementById("correct").innerHTML="";
+                    document.getElementById("showAnswer").innerHTML="<button id='crt'>Get Correct Answer</button>"
+                }
+            }
+
+            else{
+              if (arrs.includes(ans))
+              {
+              document.getElementById("correct").innerHTML="Correct Answer!!!";
+              document.getElementById("wrong").innerHTML="";
+              document.getElementById("showAnswer").innerHTML=""
+
+              }
+              else{
+                  document.getElementById("wrong").innerHTML="Wrong Answer !!!";
+                  document.getElementById("correct").innerHTML="";
+                  document.getElementById("showAnswer").innerHTML="<button id='crt'>Get Correct Answer</button>"
+              }
+
+            }
+          }
